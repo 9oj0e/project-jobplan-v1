962dc0d539910613e4eb503755a6e4d64c35661d
coderyu5523 <coderyu5523@naver.com>
2024-03-01 23:25:14 +0900

    resume ãresponseDTO 만듬

--- src/main/java/shop/mtcoding/projectjobplan/resume/ResumeResponse.java
+++ src/main/java/shop/mtcoding/projectjobplan/resume/ResumeResponse.java
@@ -1,4 +1,27 @@
 package shop.mtcoding.projectjobplan.resume;
 
+import lombok.Data;
+
+import java.sql.Timestamp;
+
 public class ResumeResponse {
-}
+    @Data
+    public static class ResumeAndUserDTO{
+        private Integer id;
+        private Integer userId; // writtenBy
+        private String title;
+        private String content; // cv, cover letter 자기소개서
+        private String career; // 회사명+경력
+
+        private String address;
+
+        // employer, 사업자 항목 nullable
+        private boolean isEmployer; // 사업자인지
+    }
+
+
+
+    }
+
+
+
