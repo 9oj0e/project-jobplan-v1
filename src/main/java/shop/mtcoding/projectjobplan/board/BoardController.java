98de1c96857b87f994e01176ae30e04ee85e2980
kjs <zeeq125@gmail.com>
2024-03-02 23:23:28 +0900

    board/uploadForm view name 설정

--- src/main/java/shop/mtcoding/projectjobplan/board/BoardController.java
+++ src/main/java/shop/mtcoding/projectjobplan/board/BoardController.java
@@ -2,9 +2,11 @@ package shop.mtcoding.projectjobplan.board;
 
 import com.sun.tools.javac.Main;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.mtcoding.projectjobplan.resume.ResumeRepository;
 import shop.mtcoding.projectjobplan.user.User;
@@ -15,9 +17,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
-
-
     private final BoardRepository boardRepository ;
+    private final HttpSession session;
 
     @GetMapping({"/", "/board"})
     public String index(HttpServletRequest request) {
