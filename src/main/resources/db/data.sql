3cdffba15a07e468484fa0824737987bc0449ae3
coderyu5523 <coderyu5523@naver.com>
2024-03-02 12:19:15 +0900

    더미 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -40,6 +40,46 @@ INSERT INTO board_tb
 (user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
 VALUES (10, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
 
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (11, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (12, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (13, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (14, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (15, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (16, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (17, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (18, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (19, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
+INSERT INTO board_tb
+(user_id, title, content, field, position, salary, opening_date, closing_date, created_at)
+VALUES (20, '제목', '2024년 신입 노예 채용', '백앤드 프론트', '신입', '최저시급', '2024-02-26', '2024-03-08', now());
+
 -- resume_tb
 
 INSERT INTO resume_tb
@@ -82,6 +122,45 @@ INSERT INTO resume_tb
 (user_id, title, content, school_name, major, education_level, career, created_at)
 VALUES (10, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
 
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (11, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (12, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (13, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (14, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (15, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (16, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (17, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (18, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (19, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
+
+INSERT INTO resume_tb
+(user_id, title, content, school_name, major, education_level, career, created_at)
+VALUES (20, '성실하고 유능한 인재입니다.', '이력서내용', '학교명', '전공', '학력', '백앤드 1년', now());
 
 
 
@@ -155,3 +234,62 @@ INSERT INTO user_tb
  is_employer, employer_id_number, business_name, created_at)
 VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '주소', '이메일',
         false, 1234, '회사명', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '주소', '이메일',
+        false, 1234, '회사명', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '서울', '이메일',
+        true, 1234, '삼성', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '대전', '이메일',
+        true, 1234, '롯데', now());
+
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '주소', '이메일',
+        false, 1234, '회사명', now());
+
+
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '대구', '이메일',
+        true, 1234, '구글', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '부산', '이메일',
+        true, 1234, '네이버', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '주소', '이메일',
+        false, 1234, '회사명', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '광주', '이메일',
+        true, 1234, '카카오', now());
+
+INSERT INTO user_tb
+(username, password, name, birthdate, gender, phone_number, address, email,
+ is_employer, employer_id_number, business_name, created_at)
+VALUES ('user', '1234', '조정현', '1996-04-01', 'M', 010-0000-0000, '주소', '이메일',
+        false, 1234, '회사명', now());
+
+
